0deaaa680330e526df6f0c6774ce18b5c8af2374
d4n13ls1104 <d4n13lsofficial@gmail.com>
2023-08-28 10:43:14 -0400

    general refactor

--- main.c
+++ main.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define BUFFER_SIZE 16
 
-void print_chunk(unsigned char* buffer, size_t file_len) {
-	size_t i;
-	for (i = 0; i < BUFFER_SIZE; i++) {
-		if (i < file_len) {
-			printf("%02X ", buffer[i]);
-		} else { printf("  "); }
-
-		if (i == 7) { printf(" "); }
+void
+print_chunk (char *buffer, size_t file_size)
+{
+	size_t i; for (i = 0; i < BUFFER_SIZE; i++)
+	{
+		if (i < file_size) printf("%02X ", buffer[i]);
+		if (i == 7) printf(" ");
 	}
 }
 
-void print_dump(FILE* file) {
-	unsigned char buffer[BUFFER_SIZE];
+void
+print_dump (FILE *file)
+{
+	char *buffer = malloc(BUFFER_SIZE);
+	if (!buffer)
+	{
+		fprintf(stderr, "Failed to allocate memory for file.");
+		exit(EXIT_FAILURE);
+	}
+
 	size_t addr = 0;
-
-	while (!feof(file)) {
+	while (!feof(file))
+	{
 		size_t bytes_read = fread(buffer, 1, BUFFER_SIZE, file);
-
 		printf("%08zX: ", addr);
 		print_chunk(buffer, bytes_read);
 
@@ -28,17 +35,20 @@ void print_dump(FILE* file) {
 	}
 }
 
-int main(int argc, char *argv[]) {
-	if (argc != 2) {
-		fprintf(stderr, "Usage: hexdmp <file>\n");
-		return 1;
+int
+main (int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <file>", argv[0]);
+		exit(EXIT_FAILURE);
 	}
 
-	const char* file_name = argv[1];
-	FILE* file = fopen(file_name, "rb");
-	if (!file) {
-		fprintf(stderr, "Failed to open file: %s\n", file_name);
-		return 1;
+	FILE *file = fopen(argv[1], "rb");
+	if (!file)
+	{
+		fprintf(stderr, "Failed to open file: %s\n", argv[1]);
+		exit(EXIT_FAILURE);
 	}
 
 	print_dump(file);
